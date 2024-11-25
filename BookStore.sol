847ab3e49aab8f07704f10ae7430738d7149d548
dreybuilds <nzaunzilani@gmail.com>
2024-11-25 13:15:10 +0300

    contract inheritance

--- BookStore.sol
+++ BookStore.sol
@@ -36,6 +36,16 @@ pragma solidity ^0.8.2;
 // use the points for the discount -
 
 
+contract A{
+    function findA() public  pure virtual returns (string memory){
+        return "contract A";
+    }
+}
+contract B is A{
+    function findA() public  pure virtual override returns (string memory){
+        return "contract B";
+    }
+}
 contract BookStore {
 
     address public owner;
@@ -98,6 +108,43 @@ contract BookStore {
         // Transfer payment to the owner - paybale == transfer(from, to, amount)
         payable(owner).transfer(msg.value);
         emit BookPurchased(_bookId, msg.sender, _quantity);
+    }
+
+}
+
+contract AdvancedBookStore is BookStore{
+    mapping (uint256 => bool)public bestsellers;
+
+    event BookRemoved(uint256 indexed bookId);
+    event BookMarkedAsBestseller(uint256 indexed bookId);
+
+    constructor(address _owner) BookStore(_owner){}
+
+    //mark book as a bestseller
+    function markAsBestseller(uint256 _bookId)public onlyOwner{
+        require(books[_bookId].price != 0, "Book does not exist");
+        bestsellers[_bookId] = true;
+        emit BookMarkedAsBestseller(_bookId);
+    }
+    function isBestseller(uint256 _bookId) public view returns (bool){
+        return bestsellers[_bookId];
+    }
+
+    function removeBook(uint256 _bookId) public onlyOwner{
+        require(books[_bookId].price != 0 , "Book does not exist");
+        delete books [_bookId];
+        
+        for (uint256 i = 0; i < bookIds.length ;i++){
+            if (bookIds[i] == _bookId){
+                bookIds[i] = bookIds[bookIds.length-1];
+                bookIds.pop();
+                break;
+            }
+        }
+        if (bestsellers[_bookId]){
+            delete bestsellers[_bookId];
+        }
+        emit BookRemoved(_bookId);
     }
 
 }
